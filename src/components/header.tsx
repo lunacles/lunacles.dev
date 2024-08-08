44bd0494452026ff3acde1980d0d4ab396c97df5
lunacles <lunahackley@gmail.com>
2024-08-08 15:42:55 -0500

    Create header directories

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,32 +1,64 @@
 import React from 'react'
 import styled from 'styled-components'
-import Props from 'styles/props'
 import NameTitle from './text'
+import List from './list'
+import Button from './button'
+import { Theme } from '../styles/theme'
 
-const StyledHeader = styled.main<Props>`
-  background-color: #282c34;
-  min-height: 10h;
-  display: flex;
-  flex-direction: row;
-  align-items: left;
-  justify-content: left;
-  font-size: calc(10px + 2vmin);
-  color: white;
-`;
+const StyledHeader = styled.header`
+  position: relative;
+  padding: 0 2rem;
+  color: ${Theme.Foreground};
+  border-bottom: ${Theme.BorderRadius * 0.5}px solid color-mix(in srgb, ${Theme.Foreground} 95%, ${Theme.White});
+
+  .navbar {
+    width: 100%;
+    height: 60px;
+    max-width: 1200px;
+    margin: 0 auto;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+`
+
+let buttons: Array<{
+  [key: string]: string
+}> = [{
+  content: 'About', href: 'about'
+}, {
+  content: 'Projects', href: 'projects'
+}, {
+  content: 'Contact', href: 'contact'
+}, {
+  content: 'Photography', href: 'photography'
+}]
 
 const Header = () => {
   return (
     <StyledHeader>
-      <header>
+      <div className='navbar'>
         <NameTitle
-          text1="luna"
-          text2=".dev"
-          color1="#ffba11"
-          color2="#b6acff"
-          weight="400"
-        ></NameTitle>
-      </header>
+          text1='luna'
+          text2='.dev'
+          color1={Theme.Primary as string}
+          color2={Theme.White as string}
+          weight='700'
+        />
+
+        <List>
+          {buttons.map((button, index) => (
+            <Button
+              type={'primary'}
+              key={index}
+              content={button.content}
+              href={button.href}
+            />
+          ))}
+        </List>
+      </div>
     </StyledHeader>
+
   )
 }
 
