09790967ae9e2e8ab87bed951c1804a3cbdf1de4
lunacles <lunahackley@gmail.com>
2024-08-08 15:43:40 -0500

    Create theme.ts

---
+++ src/styles/theme.ts
@@ -0,0 +1,17 @@
+export enum Theme {
+  Background = '#0d1117',
+  Foreground = '#161b22',
+  PrimaryContent = '#c6cdd5',
+  SecondaryContent = '#899d9b',
+  TertiaryContent = '#21262d',
+  White = '#ecf2f8',
+
+  Danger = '#fa7970',
+  Tertiary = '#faa356',
+  Special = '#7ce38b',
+  Highlight = '#a2d2fb',
+  Secondary = '#77bdfb',
+  Primary = '#cea5fb',
+
+  BorderRadius = 12,
+}
